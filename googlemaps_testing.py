6ae718e1e0878db99b960e987c5ada17ce53fa84
Vivek Kumar <vivek.skumar@gmail.com>
2023-08-18 17:19:27 +0530

    removed cred

--- googlemaps_testing.py
+++ googlemaps_testing.py
@@ -7,11 +7,13 @@ elapsed_time = []
 start_time = time.time()
 fail = []
 
+access_key = input("Please ask Vivek To Provide")
+
 with open("address.txt") as file:
     for item in file:
         print(item)
         params = urllib.parse.urlencode({
-            'access_key': 'c9a995c8844a6635acbbbe286ab4410d',
+            'access_key': access_key,
             'query': item,
             'limit': 1,
         })
@@ -22,22 +24,18 @@ with open("address.txt") as file:
         time.sleep(3)
         pos_stack_api = data.decode('utf8').replace("'", '"')
         json_output = json.loads(pos_stack_api)
-        lat, long = json_output["data"][0]['latitude'], json_output["data"][0]['longitude']
-        print(lat)
-        print(long)
-        # if len(json_output['data']) > 0:
-        #     try:
-        #         lat, long = json_output["data"][0]['latitude'], json_output["data"][0]['longitude']
-        #         print(lat)
-        #         print(long)
-        #     except TypeError:
-        #         print("\nOverloading server. Retrying after pause...\n")
-        #         elapsed_time.append(time.time() - start_time)
-        #         fail.append(1)
-        #         time.sleep(60)
-        #         continue
-        #     else:
-        #         print("\nLocation added!\n")
-        #         elapsed_time.append(time.time() - start_time)
-        #         fail.append(0)
-        #         break
+        if len(json_output['data']) > 0:
+            try:
+                lat, long = json_output["data"][0]['latitude'], json_output["data"][0]['longitude']
+            except TypeError:
+                print("\nOverloading server. Retrying after pause...\n")
+                elapsed_time.append(time.time() - start_time)
+                fail.append(1)
+                time.sleep(60)
+                continue
+        else:
+            print("\nLocation added!\n")
+            elapsed_time.append(time.time() - start_time)
+            fail.append(item)
+            break
+        print("The latitude and longitude of Address: " + str(item) + " are " + str(lat) + " and " + str(long))
